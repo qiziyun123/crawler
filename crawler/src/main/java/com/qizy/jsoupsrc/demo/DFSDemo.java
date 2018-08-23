c6db66e1c6a3e6df3ccff6116a5fd63b0f344732
qizy <qizy@localhost>
2018-08-24 07:40:18 +0800

    1

---
+++ crawler/src/main/java/com/qizy/jsoupsrc/demo/DFSDemo.java
@@ -0,0 +1,14 @@
+package com.qizy.jsoupsrc.demo;
+/**
+ * jsoup中node提供对dfs想到经典
+ * @author qizy
+ *
+ */
+public class DFSDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ crawler/src/main/java/com/qizy/jsoupsrc/demo/Node.java
@@ -0,0 +1,8 @@
+package com.qizy.jsoupsrc.demo;
+
+public class Node {
+	private int val;
+	private Node parent;
+	private Node[] children;
+	private int sibilingIndex;
+}
